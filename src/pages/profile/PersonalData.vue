f744bac75de3ab9d8ba3a6a3652ef206c6450b7a
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2024-04-03 18:04:21 +0800

    feat: 新增個人資料頁編輯欄位

--- src/pages/profile/PersonalData.vue
+++ src/pages/profile/PersonalData.vue
@@ -1,24 +1,105 @@
 <template>
-  <q-page class="layout--main row items-center justify-evenly">
+  <q-page class="layout--main q-pa-lg">
     <div>
-      <q-avatar rounded size="56px">
-        <img src="https://cdn.quasar.dev/img/avatar.png">
-      </q-avatar>
+      <div class="row items-center justify-between q-mb-lg">
+        <q-icon
+          size="36px"
+          name="eva-arrow-back-outline text-left"
+          class="col-3 content-start"
+          @click="router.go(-1)"
+        />
+        <div class="col-6 text-center">Personal Data</div>
+        <div class="col-3"></div>
+      </div>
+      <div class="text-center">
+        <q-img
+          :src="formData.imageUrl"
+          spinner-color="white"
+          style="height: 72px; max-width: 72px"
+          rounded
+          class="q-mb-md"
+        ></q-img>
+      </div>
+      <div class="q-relative-position">
+        <q-file
+          v-model="formData.image"
+          label="Pick one file"
+          filled
+          style=""
+          class="q-mb-lg"
+          @update:model-value="handleUpload()"
+        ></q-file>
+        <!-- <q-file
+          v-model="formData.image"
+          label="Pick one file"
+          filled
+          style="
+            position: absolute;
+            top: 0;
+            left: 0;
+            opacity: 0;
+            cursor: pointer;
+          "
+          @update:model-value="handleUpload()"
+          multiple
+        ></q-file> -->
+        <!-- <q-icon
+          name="attachment"
+          class="q-absolute-full clickable"
+          style="pointer-events: none"
+          size="48px"
+        ></q-icon> -->
+      </div>
       <!-- TODO: 思考這裡該如何用成編輯 -->
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-        <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
-          :rules="[val => val && val.length > 0 || 'Please type something']" />
-
-        <q-input filled type="number" v-model="age" label="Your age *" lazy-rules :rules="[
-        val => val !== null && val !== '' || 'Please type your age',
-        val => val > 0 && val < 100 || 'Please type a real age'
-      ]" />
-
-        <q-toggle v-model="accept" label="I accept the license and terms" />
-
-        <div>
-          <q-btn label="Submit" type="submit" color="primary" />
-          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      <!-- 作為一個使用者，我可以新增個人和公司的檔案 -->
+      <!--
+        1.姓名
+        2.email
+        3.年紀
+        4.職業
+        5.公司名稱
+       -->
+      <q-form
+        @submit="onSubmit"
+        @reset="onReset"
+        class="q-gutter-sm"
+        ref="myForm"
+      >
+        <q-input
+          filled
+          v-model="formData.name"
+          label="Your name *"
+          lazy-rules
+        />
+        <q-input
+          filled
+          type="email"
+          v-model="formData.email"
+          label="Your email *"
+          lazy-rules
+        />
+        <q-input
+          filled
+          type="number"
+          v-model="formData.age"
+          label="Your age *"
+          lazy-rules
+        />
+        <q-select filled v-model="model" :options="options" label="Filled" />
+        <div class="q-gutter-sm">
+          <q-btn
+            label="更新"
+            type="submit"
+            color="primary"
+            class="full-width"
+          />
+          <q-btn
+            label="清除"
+            type="reset"
+            color="primary"
+            outline
+            class="full-width q-ml-sm"
+          />
         </div>
       </q-form>
     </div>
@@ -26,9 +107,87 @@
 </template>
 
 <script setup lang="ts">
+import { useQuasar } from "quasar";
+import { ref, reactive } from "vue";
+import { useUserStore } from "../../stores/modules/user";
+import { useRouter } from "vue-router";
 
 defineOptions({
-  name: 'PersonalData'
-})
+  name: "PersonalData",
+});
 
+const $q = useQuasar();
+const router = useRouter();
+const { getCurrentUser } = useUserStore();
+
+const formData = reactive({
+  name: "",
+  age: "",
+  email: "",
+  image: null,
+  imageUrl:
+    "https://voice-verse.vercel.app/assets/default-cover-photo-3ab09c3b.png",
+});
+
+const model = ref(null);
+
+const options = reactive(["Google", "Facebook", "Twitter", "Apple", "Oracle"]);
+
+const handleUpload = () => {
+  console.log("handleUpload is triggered");
+  if (formData.image) {
+    formData.imageUrl = URL.createObjectURL(formData.image);
+  }
+};
+
+// 取得目前使用者資料
+const initUserData = () => {
+  const currentUser = getCurrentUser();
+
+  if (currentUser) {
+    console.log("@@");
+    formData.email = currentUser.email || "";
+    formData.name = currentUser.displayName || "";
+  }
+};
+
+initUserData();
+
+// const checkFileSize = (files: FileList | null) => {
+//   if (!files) return []; // 如果 files 為空，返回空陣列
+
+//   // 將 FileList 轉換為陣列
+//   const filesArray = Array.from(files);
+
+//   console.log("filesArray: ", filesArray);
+//   // 使用 filter 方法篩選符合條件的文件
+//   const filteredFiles = filesArray.filter((file) => file.size < 5120);
+
+//   return filteredFiles;
+// };
+
+// const onRejected = (rejectedEntries) => {
+//   // Notify plugin needs to be installed
+//   // https://quasar.dev/quasar-plugins/notify#Installation
+//   $q.notify({
+//     type: "negative",
+//     message: `${rejectedEntries.length} file(s) did not pass validation constraints`,
+//   });
+// };
+
+const onSubmit = () => {
+  console.log("formData: ", formData);
+  $q.notify({
+    color: "green-4",
+    textColor: "white",
+    icon: "cloud_done",
+    message: "Submitted",
+  });
+};
+
+const onReset = () => {
+  formData.name = "";
+  formData.age = "";
+  formData.email = "";
+};
 </script>
